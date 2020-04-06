689b150af972d35eb96a12be615175688dad1966
Edward Fang <edward.fang@berkeley.edu>
2020-04-06 13:54:27 -0700

    removing collinear points from csp construction

--- src/CubicSpline/CubicSpline2D.h
+++ src/CubicSpline/CubicSpline2D.h
@@ -5,12 +5,14 @@
 
 #include <vector>
 
+using namespace std;
+
 // 2-dimensional cubic spline class.
 // For technical details see: http://mathworld.wolfram.com/CubicSpline.html
 class CubicSpline2D {
 public:
     CubicSpline2D();
-    CubicSpline2D(const std::vector<double> &x, const std::vector<double> &y);
+    CubicSpline2D(const vector<double> &x, const vector<double> &y);
     double calc_x(double t);
     double calc_y(double t);
     double calc_curvature(double t);
@@ -18,10 +20,13 @@ public:
     double find_s(double x, double y, double s0);
 
 private:
-    std::vector<double> s;
+    vector<double> s;
     CubicSpline1D sx, sy;
-    void calc_s(const std::vector<double>& x,
-                const std::vector<double>& y);
+    void calc_s(const vector<double>& x,
+                const vector<double>& y);
+    vector<vector<double>> remove_collinear_points(vector<double> x,
+        vector<double> y);
+    bool are_collinear(double x1, double y1, double x2, double y2, double x3, double y3);
 };
 
 #endif //FRENET_OPTIMAL_TRAJECTORY_CUBICSPLINE2D_H
